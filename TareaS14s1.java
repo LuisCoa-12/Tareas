5c1803e0ed0cfe47142ca57ea5a2c60818a83433
LuisCoa <63749857+LuisCoa-12@users.noreply.github.com>
2020-07-15 15:32:05 -0500

    Create TareaS14s1.java

---
+++ TareaS14s1.java
@@ -0,0 +1,32 @@
+abstract class SerVivo{
+    abstract void alimentarse();
+}
+class Planta extends SerVivo{
+    public void alimentarse(){
+        System.out.println("Las plantas se alimentan de sol y agua");
+    }
+}
+abstract class Animal extends SerVivo{
+    abstract void alimentarse();
+}
+class AnimalCarnivoro extends Animal{
+    public void alimentarse(){
+        System.out.println("Los animales carnivoros se alimentan de carne");
+    }
+}
+class AnimalHervivoro extends Animal{
+    public void alimentarse(){
+        System.out.println("Los animales hervivoros se alimentan de plantas");
+    }
+}
+public class Principal{
+    public static void main(String[] args) {
+        
+        Planta sanseviera = new Planta();
+        AnimalCarnivoro leon = new AnimalCarnivoro();
+        AnimalHervivoro jirafa = new AnimalHervivoro();
+        sanseviera.alimentarse();
+        leon.alimentarse();
+        jirafa.alimentarse();
+    }
+}
